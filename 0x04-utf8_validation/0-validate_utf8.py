d2a91005b21a921151b20eacbb44241a5e264796
WENDO GITHAKA <githakawendo@students.uonbi.ac.ke>
2023-07-25 15:11:43 +0300

    t4

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,40 +1,29 @@
 #!/usr/bin/python3
-""" UTF-8 Validation """
+"""
+UTF-8 Validation
+"""
 
 
 def validUTF8(data):
     """
-    Method that determines if a given data set represents a valid
-    UTF-8 encoding.
+    data: a list of integers
+    Return: True if data is a valid UTF-8
+    encoding, else return False
     """
-    number_bytes = 0
-
-    mask_1 = 1 << 7
-    mask_2 = 1 << 6
+    byte_count = 0
 
     for i in data:
-
-        mask_byte = 1 << 7
-
-        if number_bytes == 0:
-
-            while mask_byte & i:
-                number_bytes += 1
-                mask_byte = mask_byte >> 1
-
-            if number_bytes == 0:
-                continue
-
-            if number_bytes == 1 or number_bytes > 4:
+        if byte_count == 0:
+            if i >> 5 == 0b110 or i >> 5 == 0b1110:
+                byte_count = 1
+            elif i >> 4 == 0b1110:
+                byte_count = 2
+            elif i >> 3 == 0b11110:
+                byte_count = 3
+            elif i >> 7 == 0b1:
                 return False
-
         else:
-            if not (i & mask_1 and not (i & mask_2)):
-                    return False
-
-        number_bytes -= 1
-
-    if number_bytes == 0:
-        return True
-
-    return False 
+            if i >> 6 != 0b10:
+                return False
+            byte_count -= 1
+    return byte_count == 0
